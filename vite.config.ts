4121bab40ff144736441ba89d6bfaa6efafd1626
bustawin <xavier@bustawin.com>
2024-02-13 20:49:10 +0100

    Link bootstrap

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ export default defineConfig({
     alias: {
       // even if we have tsconfigPaths we require this one here due to sass
       '@jutils': path.resolve(__dirname, 'jutils', 'src'),
+      '@bootstrap': path.resolve(__dirname, 'node_modules/bootstrap/scss'),
     },
   },
 })
