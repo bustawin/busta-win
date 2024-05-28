cf6374cf02c6bfd581b3d8d3ab2f52d794b059a0
bustawin <xavier@bustawin.com>
2024-05-28 08:05:21 +0200

    Update cache

--- ui/utils/cache.ts
+++ ui/utils/cache.ts
@@ -1,5 +1,10 @@
+const ttlInHours = 2
+const ttlInSeconds = ttlInHours * 60 * 60
+const proxyTtl = ttlInSeconds
+const browserTtl = ttlInSeconds / 2
+
 export const loaderCache = {
-  'Cache-Control': `public, max-age=${30 * 60}, s-maxage=${30 * 60}`,
+  'Cache-Control': `public, max-age=${browserTtl}, s-maxage=${proxyTtl}`,
 }
 
 export const documentCache = loaderCache
