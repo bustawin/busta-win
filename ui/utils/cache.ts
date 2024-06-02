638c33470781eeec208045b0528befea41482781
bustawin <xavier@bustawin.com>
2024-06-02 18:45:03 +0200

    Increase cache times

--- ui/utils/cache.ts
+++ ui/utils/cache.ts
@@ -1,7 +1,8 @@
-const ttlInHours = 2
-const ttlInSeconds = ttlInHours * 60 * 60
-const proxyTtl = ttlInSeconds
-const browserTtl = ttlInSeconds / 2
+const proxyTtlInHours = 12
+const proxyTtl = proxyTtlInHours * 60 * 60
+
+const browserTtlInHours = 2
+const browserTtl = browserTtlInHours * 60 * 60
 
 export const loaderCache = {
   'Cache-Control': `public, max-age=${browserTtl}, s-maxage=${proxyTtl}`,
