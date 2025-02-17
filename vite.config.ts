03d045d2950db2369c6c87982732e7d0bf7592b3
bustawin <xavier@bustawin.com>
2025-02-17 22:52:41 +0100

    feat(framework): use optimizeDeps

--- vite.config.ts
+++ vite.config.ts
@@ -4,14 +4,19 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 import path from 'path'
 
 export default defineConfig({
-  plugins: [remix({ appDirectory: 'ui' }), tsconfigPaths()],
+  plugins: [remix({
+    appDirectory: 'ui', future: {
+      // from https://remix.run/docs/en/main/guides/dependency-optimization
+      unstable_optimizeDeps: true
+    }
+  }), tsconfigPaths()],
   resolve: {
     alias: {
       // even if we have tsconfigPaths we require this one here due to sass
       '@jutils': path.resolve(__dirname, 'jutils', 'src'),
       '@bootstrap': path.resolve(__dirname, 'node_modules/bootstrap/scss'),
       '@highlight': path.resolve(__dirname, 'node_modules/highlight.js/scss'),
-      '@style': path.resolve(__dirname, 'ui/styles/style.sass'),
-    },
-  },
+      '@style': path.resolve(__dirname, 'ui/styles/style.sass')
+    }
+  }
 })
