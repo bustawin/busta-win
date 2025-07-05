44fe4fff8e5043eddac6dbef764f055506db49f2
bustawin <xavier@bustawin.com>
2025-07-05 17:59:41 +0200

    feat(post): Add planning-effective-meetings

--- vite.config.ts
+++ vite.config.ts
@@ -4,24 +4,31 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 import path from 'path'
 
 export default defineConfig({
-  plugins: [remix({
-    appDirectory: 'ui', future: {
-      // from https://remix.run/docs/en/main/guides/dependency-optimization
-      unstable_optimizeDeps: true,
-      v3_fetcherPersist: true,
-      v3_relativeSplatPath: true,
-      v3_throwAbortReason: true,
-      v3_lazyRouteDiscovery: true,
-      v3_singleFetch: true,
-    }
-  }), tsconfigPaths()],
+  plugins: [
+    remix({
+      appDirectory: 'ui',
+      future: {
+        // from https://remix.run/docs/en/main/guides/dependency-optimization
+        unstable_optimizeDeps: true,
+        v3_fetcherPersist: true,
+        v3_relativeSplatPath: true,
+        v3_throwAbortReason: true,
+        v3_lazyRouteDiscovery: true,
+        v3_singleFetch: true,
+      },
+    }),
+    tsconfigPaths(),
+  ],
   resolve: {
     alias: {
       // even if we have tsconfigPaths we require this one here due to sass
       '@jutils': path.resolve(__dirname, 'jutils', 'src'),
       '@bootstrap': path.resolve(__dirname, 'node_modules/bootstrap/scss'),
       '@highlight': path.resolve(__dirname, 'node_modules/highlight.js/scss'),
-      '@style': path.resolve(__dirname, 'ui/styles/style.sass')
-    }
-  }
+      '@style': path.resolve(__dirname, 'ui/styles/style.sass'),
+    },
+  },
+  server: {
+    allowedHosts: ['bustabook.local'],
+  },
 })
